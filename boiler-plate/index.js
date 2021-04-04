1d15f5ff39e1572f65dca663a18766fde548d1c1
kermit yang <kermit1727@gmail.com>
2021-04-04 18:22:26 +0000

    implemented post processing

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -3,6 +3,15 @@ const app = express();
 const port = 8444;
 const config = require("./config/key");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
 
 // mongo db 연결
 mongoose.connect(config.mongoURI, {
@@ -10,9 +19,33 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('Mongo connect...!'))
   .catch(err => console.log(err));
 
+// get요청이 올경우 /에서 응답해줄 내용
 app.get('/', (req, res) => res.send('<h1>Welcome!!</h1><p>This is <mark>kermit</mark> World!!</p>'));
 
+// post요청이 올경우(회원가입)
+app.post('/register', (req, res) => {
+  // 회원가입할때 필요한 정보들을 client에서 가져오면
+  // 가져온 데이터를 DB에 저장한다.
+  
+  // userSchema 인스턴스 생성
+  // 인자로 요청값의 body(post)값을 전달
+  const user = new User(req.body);
+  
+  // 전달받은 값을 DB에 저장
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err});
+    
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+
+// express서버 실행
 app.listen(port, () => {
   console.log(`start kermit server on ${port} port!`)
-  console.log(`Let's go!!`);
+  console.log(`내일도 출근!`);
 });
+
+
--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -30,3 +30,12 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+
+// 해닫코드가 실행되야 스키마가 등록된다.
+// 첫번째 인자 -> 컬렉션이름, 소문자치환 후 복수형으로 변환(User -> users)
+// 두번째 인자 -> 등록할 스키마
+// 세번째 인자 -> 첫번째 경우가 싫다면 여기에 사용할 이름 직접등록!
+const User = mongoose.model('User', userSchema);
+
+module.exports = {User};
