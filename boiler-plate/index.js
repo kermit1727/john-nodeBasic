8187fffd45f6e2c37567758009427a8f05ff792c
kermit yang <kermit1727@gmail.com>
2021-04-04 16:29:48 +0000

    connect mongoDB

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,6 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8444;
+const config = require("./config/key");
+const mongoose = require("mongoose");
+
+// mongo db 연결
+mongoose.connect(config.mongoURI, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('Mongo connect...!'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('<h1>Welcome!!</h1><p>This is <mark>kermit</mark> World!!</p>'));
 
